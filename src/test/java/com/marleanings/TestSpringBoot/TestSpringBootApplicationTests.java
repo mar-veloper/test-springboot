cb699098e196dde7f6d8e8e9625bfdafd2d9160e
mar-veloper <jonmar21yan@gmail.com>
2024-02-23 13:21:40 +0100

    Init: SpringBoot project

---
+++ src/test/java/com/marleanings/TestSpringBoot/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marleanings.TestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
